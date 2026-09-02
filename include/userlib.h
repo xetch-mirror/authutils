878ecce6e6f95b1ce8c8cc0827faadffbf45118a
Xetch <babyroselegaspi01@gmail.com>
2026-09-02 17:02:32 +0800

    Update userlib.h

--- include/userlib.h
+++ include/userlib.h
@@ -1,23 +1,22 @@
 #ifndef USERLIB_H
 #define USERLIB_H
 
-#define USER_MAX_ACCOUNTS 16
-#define USER_NAME_LEN     32
-#define USER_HASH_LEN     32
+#define USER_NAME_LEN 32
+#define USER_PASS_LEN 32
 
 typedef struct {
     int  uid;
     char name[USER_NAME_LEN];
-    char passhash[USER_HASH_LEN];
     int  is_admin;
-    int  in_use;
 } user_t;
 
-void   userlib_init(void);
-int    userlib_add(const char *name, const char *passhash, int is_admin);
+void userlib_init(const char *ini_path);
 user_t *userlib_find_by_name(const char *name);
 user_t *userlib_find_by_uid(int uid);
-int    userlib_current_uid(void);
-void   userlib_set_current_uid(int uid);
+const char *userlib_get_password(const char *name);
+int  userlib_set_password(const char *name, const char *new_password);
+int  userlib_write(const char *ini_path);
+int  userlib_current_uid(void);
+void userlib_set_current_uid(int uid);
 
 #endif
